3dd40437867a72e97e7d4283d4bb5ac31ba4e0ff
Wade Kallhoff <wade@HAL-9000-Core-Unit.local>
2019-04-26 17:30:10 -0500

    Initial commit

---
+++ readme.md
@@ -0,0 +1,54 @@
+# phaser-electron-typescript-parcel [![Build Status](https://travis-ci.org/distantcam/phaser-electron-typescript-parcel.svg?branch=master)](https://travis-ci.org/distantcam/phaser-electron-typescript-parcel)
+
+A minimal template with the following things.
+
+- [Phaser](https://phaser.io/) 🕹️ Desktop and Mobile HTML5 game framework
+- [Electron](https://electronjs.org/) ⚛️ Build cross platform desktop apps with JavaScript, HTML, and CSS
+- [Parcel](https://github.com/parcel-bundler/parcel) 📦 Blazing fast, zero configuration web application bundler
+- [TypeScript](https://www.typescriptlang.org/) ⌨️ is a typed superset of JavaScript that compiles to plain JavaScript.
+
+## Installation
+
+* `git@github.com:distantcam/phaser-electron-typescript-parcel.git`
+* `cd phaser-electron-typescript-parcel`
+* `yarn`
+
+## Usage
+
+### Cleaning
+Run these commands to clean up the directory
+``` bash
+# Cleans up the build and dist folders
+yarn clean
+
+# Cleans up the builds, build cache, and node modules
+yarn superclean
+```
+
+### Development mode
+Run these commands to build and run the Electron app
+``` bash
+# Parcel bundles the code
+$ yarn build
+
+# Parcel bundles the code and watches for changes
+$ yarn watch
+
+# Run the electron app
+$ yarn app
+
+# Run the electron app with options for a debugger to attach to the render process
+$ yarn debug
+
+# To debug the app in VS Code you can
+# - use yarn debug and run 'Electron: Renderer' in the debugger
+# or
+# - run 'Electron: All' in the debugger
+```
+
+### Production mode and packaging app
+Run this command to bundle code in production mode
+``` bash
+# Create executables
+$ yarn dist
+```
---
+++ src/features/menu/game-menu.ts
@@ -0,0 +1,59 @@
+export class GameMenu extends Phaser.GameObjects.GameObject {
+    private items: GameMenuItem[];
+    private menuItemHeight: number = 20;
+    private x: number;
+    private y: number;
+
+    constructor(scene: Phaser.Scene, x: number, y: number) {
+        super(scene, "GameMenu");
+        this.items = [];
+        this.x = x;
+        this.y = y;
+    }
+
+    public addMenuItem(title: string, action: () => void): GameMenu {
+        const y = this.y + (this.items.length * this.menuItemHeight);
+        this.items.push(new GameMenuItem(this.scene, this.x, y, title, action));
+        return this;
+    }
+}
+
+class GameMenuItem extends Phaser.GameObjects.GameObject {
+    private x: number;
+    private y: number;
+    private title: string;
+    private action: () => void;
+    private text: Phaser.GameObjects.Text;
+
+    constructor(scene: Phaser.Scene, x: number, y: number, title: string, action: () => void) {
+        super(scene, "GameMenuItem");
+        this.scene = scene;
+        this.title = title;
+        this.action = action;
+        this.x = x;
+        this.y = y;
+
+        this.text = this.scene.add.text(x, y, title, { fill: "#0f0"});
+        this.text.setOrigin(0.5);
+        this.text.setInteractive({ useHandCursor: true })
+        .on("pointerover", () => this.enterButtonHoverState() )
+        .on("pointerout", () => this.enterButtonRestState() )
+        .on("pointerdown", () => this.enterButtonActiveState() )
+        .on("pointerup", () => {
+            this.enterButtonHoverState();
+            action();
+        });
+    }
+
+    private enterButtonHoverState() {
+        this.text.setStyle({ fill: "#ff0"});
+    }
+
+    private enterButtonRestState() {
+        this.text.setStyle({ fill: "#0f0", align: "right"});
+    }
+
+    private enterButtonActiveState() {
+        this.text.setStyle({ fill: "#0ff"});
+    }
+}
